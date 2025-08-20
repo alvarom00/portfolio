1f4b15eb3aa437dd0430ddb171d56b8730891b42
alvarom00 <alvaromanterola00@gmail.com>
2025-08-20 18:52:00 -0300

    -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const navItems = [
   { key: "home", href: "#hero" },
   { key: "about", href: "#about" },
   { key: "skills", href: "#skills" },
+  { key: "projects", href: "#projects" },
   { key: "resume", href: "#resume" },
   { key: "contact", href: "#contact" },
 ];
